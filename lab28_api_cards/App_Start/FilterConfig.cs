da74e03ec589e1eb3ac39723abf622180f2aacbe
saunlani <saunlani@gmail.com>
2018-06-06 14:21:01 -0400

    Add project files.

---
+++ lab28_api_cards/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace lab28_api_cards
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ lab28_api_cards/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab28_api_cards.MvcApplication" Language="C#" %>
